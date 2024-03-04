4b5f7e84e866f4cba177a73d78f90f7503136e5b
o.chebotar <o.chebotar@bk.ru>
2024-03-05 03:07:06 +1100

    part2

--- src/Account.java
+++ src/Account.java
@@ -1,14 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 
+class NothingToUndo extends Exception{}
+interface Command{
+    public void perform();
+}
 enum Currency {RUB, USD, EURO, CNY}
+enum TypeAccount {ORDINARY, PREMIUM}
 public class Account {
     private String name;
     private HashMap<Currency, Integer> curramount;
+    private TypeAccount typeAccount;
+    private Deque<Command> commands = new ArrayDeque<>();
 
     private Account() {}
 
     public Account(String name) {
+        this(name, TypeAccount.ORDINARY);
+    }
+
+    public Account(String name, TypeAccount typeAccount) {
         this.setName(name);
+        this.setTypeAccount(typeAccount);
         this.curramount = new HashMap<>();
     }
 
@@ -17,7 +31,20 @@ public class Account {
     }
 
     public void setName(String name) {
+        if (name == null || name.isEmpty()) throw new IllegalArgumentException();
+        String oldName = this.name;
+        this.commands.push(()->{this.name = oldName;});
         this.name = name;
+    }
+
+    public TypeAccount getTypeAccount() {
+        return typeAccount;
+    }
+
+    public void setTypeAccount(TypeAccount typeAccount) {
+        TypeAccount oldTypeAccount = this.typeAccount;
+        this.commands.push(()->{this.typeAccount = oldTypeAccount;});
+        this.typeAccount = typeAccount;
     }
 
     public void printCurrAmount() {
@@ -30,6 +57,16 @@ public class Account {
 
     public void setAmount(Currency currency, Integer val) {
         if(val < 0) throw new IllegalArgumentException();
+        if (curramount.containsKey(currency)) {
+            this.commands.push(()->{this.curramount.put(currency, val);});
+        } else {
+            this.commands.push(()->{this.curramount.remove(currency);});
+        }
         this.curramount.put(currency, val);
+    }
+    public Account undo() throws NothingToUndo {
+        if (commands.isEmpty()) throw new NothingToUndo();
+        commands.pop().perform();
+        return this;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NothingToUndo {
         Account a1 = new Account("account1");
-        a1.setName("newaccount1");
         a1.setAmount(Currency.RUB, 1000);
         a1.setAmount(Currency.CNY, 2000);
+        a1.setAmount(Currency.USD, 3000);
+        a1.setName("account2");
+        a1.setTypeAccount(TypeAccount.PREMIUM);
+        System.out.println(a1.getName());
         a1.printCurrAmount();
+        System.out.println(a1.getTypeAccount());
+        System.out.println("-------------------");
+        a1.undo();
+        a1.undo();
+        a1.undo();
+        System.out.println(a1.getName());
+        a1.printCurrAmount();
+        System.out.println(a1.getTypeAccount());
     }
 }
